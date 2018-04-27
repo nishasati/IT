f1672b5d3f0a47ef395998c90de41b4082182d1f
nishasati <38490228+nishasati@users.noreply.github.com>
2018-04-27 09:31:53 +0530

    Delete file.php

--- file.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo $_FILES["uploadfile"]["name"]."has been uploaded.<br>";
-move_uploaded_file($_FILES["uploadfile"]["tmp_name"],
-"/var/www/html/upload/".$_FILES["uploadfile"]["name"]);
-echo "stored in;"."/var/www/html/upload/" . $_FILES["uploadfilename"]["name"];
-?>
-
